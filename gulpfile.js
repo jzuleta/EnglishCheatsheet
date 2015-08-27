7d0776b058aa44d6bd4480feb2741c27b534f837
jzuleta <j.zuletagt@gmail.com>
2015-08-26 21:06:51 -0300

    Prefijos CSS
    Se ajustaron los prefijos para el estilo en general de la aplicación.

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+﻿var gulp = require("gulp"),
+    rename = require("gulp-rename"),
+    autoprefixer = require("gulp-autoprefixer");
+
+gulp.task("add-prefixes", function () {
+    gulp.src("resources/css/dev-css.css")
+        .pipe(autoprefixer({
+            cascade: false,
+            remove: true
+        }))
+        .pipe(rename("app.css"))
+        .pipe(gulp.dest("resources/css/"));
+});
